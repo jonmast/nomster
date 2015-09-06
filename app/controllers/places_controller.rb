b623e0ee1ad0fff302aa4fe9166e92e9c6c44b82
Jonathan Mast <jon@padorpers.com>
2015-09-05 20:31:45 -0400

    Add place create action

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,5 +4,21 @@ class PlacesController < ApplicationController
   end
 
   def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    if place.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :address, :description)
   end
 end
