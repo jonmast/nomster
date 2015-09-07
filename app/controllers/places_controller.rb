e2c54e8bbd394bb3ef35bcff2a2c52e319708c27
Jonathan Mast <jon@padorpers.com>
2015-09-07 19:24:17 -0400

    Secure edit and update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   def index
     @places = Place.page(params[:page]).per(10)
   end
@@ -23,10 +23,16 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
     if @place.update_attributes(place_params)
       redirect_to root_path
     else
