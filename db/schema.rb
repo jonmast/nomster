46a4bd4df1758a91b95d1a9a37891d9112326042
Jonathan Mast <jon@padorpers.com>
2015-09-08 17:17:50 -0400

    Add comments model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908001901) do
+ActiveRecord::Schema.define(version: 20150908210634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "message"
+    t.string   "rating"
+    t.integer  "user_id"
+    t.integer  "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["place_id"], name: "index_comments_on_place_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -47,5 +59,7 @@ ActiveRecord::Schema.define(version: 20150908001901) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "places"
+  add_foreign_key "comments", "users"
   add_foreign_key "places", "users"
 end
