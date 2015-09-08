83f504e6deacf9ad30cbd432050c3b87168ec2f1
Jonathan Mast <jon@padorpers.com>
2015-09-08 17:55:22 -0400

    Add comments controller and implement add comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
   get '/page/:page', to: 'places#index'
-  resources :places, only: [:new, :create, :show, :edit, :update]
+  resources :places, except: :index do
+    resources :comments, only: :create
+  end
 end
