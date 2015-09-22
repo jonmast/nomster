169d44e451341b779cf735c820c0a7f246d0e1da
Jonathan Mast <jon@padorpers.com>
2015-09-22 08:17:50 -0400

    Add test for comment creation

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'must be signed in' do
+    place = FactoryGirl.create(:place)
+    assert_no_difference 'Comment.count' do
+      post :create, place_id: place.id, comment: {
+        message: 'Nice place',
+        rating: '5_stars'
+      }
+    end
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'adds a comment' do
+    place = FactoryGirl.create(:place)
+    user = FactoryGirl.create(:user)
+    sign_in user
+    assert_difference 'Comment.count' do
+      post :create, place_id: place.id, comment: {
+        message: 'Nice place',
+        rating: '5_stars'
+      }
+    end
+    assert_redirected_to place_path(place)
+  end
 end
--- test/factories.rb
+++ test/factories.rb
@@ -3,4 +3,17 @@ FactoryGirl.define do
     sequence(:email) { |n| "user#{n}@example.com" }
     password 'Passw@rd'
   end
+
+  factory :comment do
+    sequence(:message) { |n| "Comment #{n}" }
+    rating { Comment::RATINGS.values.sample }
+    user
+  end
+
+  factory :place do
+    sequence(:name) { |n| "Place #{n}" }
+    description 'Simply awesome place!'
+    address 'MyStreet Mytown, St 12345'
+    user
+  end
 end
